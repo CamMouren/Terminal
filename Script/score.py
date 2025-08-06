8b14aa899a8cebda92ec73058d2519080e4bf0a2
Kratsymac <16133026+CamMouren@users.noreply.github.com>
2025-08-06 22:23:31 +0200

    work on score algoryth, differencing by enemy class also work on main file becaus i miss to create a new branch ;)

--- Script/score.py
+++ Script/score.py
@@ -1,6 +1,7 @@
 import pyxel
 import debug
 import player
+import enemi
 
 
 class Score:
@@ -8,24 +9,35 @@ class Score:
         '''
         Initialiser les variables nécessaires au calcul des points.
         '''
-        self._score          = 0
-        self._killed_enemies = 0
+        self.enemy_type=0
+        self._killed_enemies=0
+        self._killed_enemies_1 = 0
+        self._killed_enemies_2 = 0
+        self._killed_enemies_3 = 0
+        self._points_killed_enemies_1=10
+        self._points_killed_enemies_2=20
+        self._points_killed_enemies_3=50
+        self._score=0
         self.debug_pos = 256 // 2 - 10
 
     def update_killed_enemies_count(self):
         '''
         Compter le nombre d'ennemis tuer.
         '''
-        self._killed_enemies += 1
-
-    def get_killed_enemies_count(self):
-        return self._killed_enemies
+        self.enemy_type=enemi.choose_enemi()
+        if self.enemy_type==1:
+            self._killed_enemies_1 += 1
+        elif self.enemy_type==2:
+            self._killed_enemies_2+=1
+        elif self.enemy_type==5: self._killed_enemies_3+=5
     
     def update_score(self):
         '''
         Calculer le score.
         '''
-        self._score = self._killed_enemies * 10
+        self.update_killed_enemies_count()
+        self._score = (self._killed_enemies_1*self._points_killed_enemies_1)+(self._killed_enemies_2*self._points_killed_enemies_2)+(self._killed_enemies_3*self._points_killed_enemies_3)
+        self._killed_enemies=self._killed_enemies_1+self._killed_enemies_2+ self._points_killed_enemies_3
 
     def get_score(self):
         return self._score
@@ -41,9 +53,13 @@ class Score:
         if debug.debug_mode == True:
             pyxel.text(player.player.x - self.debug_pos,
                        player.player.y - self.debug_pos + 7,
-                       f"Killed enemies: {self._killed_enemies}", pyxel.COLOR_YELLOW)   
+                       f"Killed enemies: {self._killed_enemies=}", pyxel.COLOR_YELLOW)   
     def reset(self):
         self._score          = 0
-        self._killed_enemies = 0
+        self._killed_enemies_1 = 0
+        self._killed_enemies_2 = 0
+        self._killed_enemies_3 = 0
+        self._killed_enemies=0
+
 
 score = Score()
